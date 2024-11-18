ea19529b43f1b835b6fee01eeca321e63cddf963
c9kisben <c9kisben@vasvari.org>
2024-11-18 08:34:51 +0100

    2. feladat kész

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+import express from "express"
 
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res, next) => {
+    res.send("Hi, there!");
+})
+
+app.get("/express", (req, res, next) =>{
+    res.send("Az Express egy minimalista webes keretrendszer, amely a Node.js-hez készült.")
+})
+
+app.get("/greeting", (req, res, next) =>{
+    res.send("Hello,  Benedek Kiss  ")
+})
+
+app.get("/nodejs", (req, res, next) =>{
+    res.send("A Node.js egy olyan szerveroldali JavaScript futtatókörnyezet, amely a V8 JavaScript motorra épül.")
+})
+app.listen(PORT);
